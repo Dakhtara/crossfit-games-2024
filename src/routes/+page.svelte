7d5261a57c70af115ad50b44c04fa4e4733bf023
Anthony <anthony.matignon@gmail.com>
2024-03-15 15:00:16 +0100

    Update width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,14 @@
   let secondWod = data["24.2"];
 </script>
 
-<div class="mx-auto">
+<div class="mx-auto w-3/4">
   <h1 class="text-3xl text-center pt-4">Résultats box Games 2024</h1>
 
   <div class="grid grid-cols-2 gap-x-4 mt-8">
     <div>
       <h2 class="text-2xl text-center">24.1</h2>
       <div class="bg-gray-100 rounded-lg border-gray-200 border">
-        <table class=" w-full table-auto">
+        <table class=" w-full table-fixed">
           <thead class="bg-gray-200">
             <tr>
               <th class="text-left pl-1 py-2">Prénom</th>
@@ -38,7 +38,7 @@
         
       <h2 class="text-2xl text-center">24.2</h2>
       <div class="bg-gray-100 rounded-lg border-gray-200 border">
-        <table class="w-full table-auto">
+        <table class="w-full table-fixed">
         <thead class="bg-gray-200">
           <tr>
             <th class="text-left py-2 pl-1">Prénom</th>
